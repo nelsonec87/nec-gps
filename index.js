ff22a6e94d939265c3304349d0f4b77b1352ff70
nelson <nelsonec87@gmail.com>
2017-10-02 12:02:07 -0300

    adf

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let http = require('http');
-
+let port = (process.env.PORT || 5000);
 let server = new http.Server((req, res) => {
     console.log(req.headers);
     res.write(JSON.stringify(req.headers));
     res.end();
 });
 
-server.listen(80);
+server.listen(port);
+console.log('port', port);
